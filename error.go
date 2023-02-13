a23a3b73fc0fa0396e7593639d63501c664b5567
Kininaru <shiftregister233@outlook.com>
2023-02-13 18:41:10 +0800

    fix: server name error

--- error.go
+++ error.go
@@ -3,6 +3,7 @@ package topazsdk
 import "errors"
 
 var (
+	ErrInvalidSelfName    = errors.New("invalid self name(cannot be empty)")
 	ErrWrongPassword      = errors.New("wrong password")
 	ErrInvalidTopazServer = errors.New("invalid topaz server")
 	ErrUserNotExists      = errors.New("user not exists")
